7723855b6a395e9f94be710e85cf51e55f0f0035
RazGavrieli <razgavrieli@gmail.com>
2022-03-22 12:24:57 +0200

    tests

--- Test.cpp
+++ Test.cpp
@@ -1,1 +1,14 @@
 // CREATE TEST CLASS
+#include "doctest.h"
+#include "Notebook.hpp"
+using namespace ariel;
+
+#include <string>
+
+TEST_CASE("Bad input") {
+        SUBCASE("Out of bounds") {
+            CHECK_THROWS(/*READ MORE THAN 100 CHARS*/);
+            CHECK_THROWS(/*READ FROM THE 60TH CHAR MORE THAN 40 CHARS*/);
+        }
+
+}
