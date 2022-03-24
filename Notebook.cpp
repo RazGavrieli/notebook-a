b674bd37183bdb51ac286b98c7193a69109ca59a
Raz <razgavrieli@gmail.com>
2022-03-24 13:17:18 +0200

    V

--- Notebook.cpp
+++ Notebook.cpp
@@ -1,35 +1,142 @@
 #include <string>
 #include "Direction.hpp"
 #include <iostream>
-
+#include <vector>
 #include "Notebook.hpp"
-namespace ariel {
-    
-            Notebook::Notebook()
-            {
-                // declare DEFEINED amount of pages 
 
+#define INIT_SIZE 5
+
+
+class Page {
+    std::vector<std::string> rows;
+    const std::string emptyLine = "____________________________________________________________________________________________________";  
+    const int lineSize = 100;
+    void allocateRows(int r) {
+        /**
+         * @brief private function that allocates new rows for the page
+         * 
+         */
+        for (int i = rows.size(); i<=r; i++) {
+            rows.push_back(emptyLine);
+        }
+    }
+    public:
+        Page() {
+            /**
+            * @brief 
+            * vector of strings has already been declared in the fields of the class. (objects initialization)
+            * In this constructor we will declare defined amount of rows. Each row is a string with 100 empty characters.
+            */ 
+
+            for (int i = 0; i<INIT_SIZE; i++) {
+                rows.push_back(emptyLine);
             }
-    
-            void Notebook::write(int page, int row, int col, Direction dir, std::string text) {
-                // add implementation
-                std::cout << "test\n";
+        }
+        int getsize() {
+            /**
+             * @brief getter for the amount of rows in the page 
+             */
+
+            return rows.size();
+        }
+        std::string getrow(int r) {
+            /**
+             * @brief getter for a specific row in the page (string type, length 100)
+             * if the row is bigger than the amount of rows in the page, allocate new rows for it.
+             */
+            if (r>rows.size()) {
+                allocateRows(r);
+                return rows.at(r);
             }
-            std::string Notebook::read(int page, int row, int col, Direction dir, int len) {
-                // add implementation
-                return "test returned";
+            return rows.at(r);
+        }
+        std::string getcol(int c) {
+            if (c<0 || c>100) {
+                throw std::runtime_error("collums must be an int between 0-100!");
             }
-            void Notebook::erase(int page, int row, int col, Direction dir, int len) {
-                // add implementation
+            std::string col;
+            for (int i = 0; i < rows.size(); i++)
+            {
+                col += getrow(i)[c];
             }
-            void Notebook::show(int page) {
-                // add implementation
+            return col;
+            
+        }
+        void replaceline(int r, std::string newline) {
+            if (newline.size()!=lineSize) {
+                throw std::runtime_error("new line size has to be exactly 100!");
             }
-    
+            if (r>rows.size()) {
+                allocateRows(r);
+                rows[r] = newline;
+            } else {
+                rows[r] = newline;
+            }
+        }
+};
+
+namespace ariel {
+    class Notebook {
+            std::vector<Page> pages;
+            const std::string emptyLine = "____________________________________________________________________________________________________";  
+            const int lineSize = 100;
+            public:
+                Notebook()
+                {
+                for (int i = 0; i<INIT_SIZE; i++) {
+                    Page newPage;
+                    pages.push_back(newPage);
+                }
+                }
+        
+                void write(int page, int row, int col, Direction dir, std::string text) {
+
+
+                    if (page>pages.size()) {
+                        // allocate new pages
+                    } 
+
+                    if (dir==Direction::Horizontal) {
+                        if (lineSize-col<text.size()) {
+                            throw std::runtime_error("text is out of bounds!");
+                        }
+                        std::string oldLine = pages.at(page).getrow(row);
+                        std::string newLine = emptyLine;
+                        int j = 0;
+                        for (int i = 0; i<lineSize; i++) {
+                            if (i>=col&&i<col+text.size()) {
+                                if (oldLine[i]=='_') {
+                                    newLine[i]=text[j];
+                                    j++;
+                                } else {
+                                    throw std::runtime_error("text is overlapping!");
+                                }
+                            } else {
+                                newLine[i]=oldLine[i];
+                            }
+                        }
+                        pages.at(page).replaceline(row, newLine);
+                        std::cout << newLine;
+                    } else {
+                        // DIRECTION IS VERTICAL!
+                    }
+                    
+                }
+                std::string read(int page, int row, int col, Direction dir, int len) {
+                    // add implementation
+                    return "test returned";
+                }
+                void erase(int page, int row, int col, Direction dir, int len) {
+                    // add implementation
+                }
+                void show(int page) {
+                    // add implementation
+                }
+    };
 }
 
-    Page::Page() {
-        // declare array of strings (DEFINED amount of rows)
-        // length of each string is 100. 
-        // each string is initialized with "_" in each char.
-    } 
+int main() {
+    ariel::Notebook notebook;
+    notebook.write(1, 1, 98, ariel::Direction::Horizontal, "test");
+    return 0;
+}
